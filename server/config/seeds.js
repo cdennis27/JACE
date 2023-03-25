7c7d353e48037109db72053cebeea1c38e7ebfff
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-03-24 20:37:30 -0400

    fixed personal branch issue and updated pictures and names of product items

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,11 +5,11 @@ db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: 'Signature Dishes' },
+    { name: 'Eggs Benedict' },
+    { name: 'Pancake Dishes' },
+    { name: 'Beverages' },
+    { name: 'Add-Ons' }
   ]);
 
   console.log('categories seeded');
@@ -18,112 +18,212 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: "New York Striploin & Eggs",
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+      image: 'NEW-YORK-STRIPLOIN-n-EGGS.jpg',
       category: categories[0]._id,
       price: 2.99,
       quantity: 500
     },
     {
-      name: 'Canned Coffee',
+      name: "Big Breakfast",
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+      image: 'ES-EGGSMART-BIG-BREAKFAST.jpg',
       category: categories[0]._id,
       price: 1.99,
       quantity: 500
     },
     {
-      name: 'Toilet Paper',
-      category: categories[1]._id,
+      name: "Classic Breakfast",
+      category: categories[0]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
+      image: 'Classic-Breakfast.jpg',
       price: 7.99,
       quantity: 20
     },
     {
-      name: 'Handmade Soap',
-      category: categories[1]._id,
+      name: "Keto Breakfast",
+      category: categories[0]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
+      image: 'KETO-BREAKFAST-PLATE.jpg',
       price: 3.99,
       quantity: 50
     },
     {
-      name: 'Set of Wooden Spoons',
-      category: categories[1]._id,
+      name: "Signature Hash",
+      category: categories[0]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
+      image: 'SIGNATURE-HASH.jpg',
       price: 14.99,
       quantity: 100
     },
     {
-      name: 'Camera',
-      category: categories[2]._id,
+      name: "Tri-Fecta",
+      category: categories[0]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
+      image: 'TRI-FECTA.jpg',
       price: 399.99,
       quantity: 30
     },
     {
-      name: 'Tablet',
-      category: categories[2]._id,
+      name: "Eggs Benedict",
+      category: categories[1]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
+      image: 'EGGS-BENEDICT.jpg',
       price: 199.99,
       quantity: 30
     },
     {
-      name: 'Tales at Bedtime',
-      category: categories[3]._id,
+      name: "Baja Benedict",
+      category: categories[1]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
+      image: 'BAJA.jpg',
       price: 9.99,
       quantity: 100
     },
     {
-      name: 'Spinning Top',
-      category: categories[4]._id,
+      name: "Meat Lovers Benedict",
+      category: categories[1]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
+      image: 'ES-EGGS-BENNIE_MEAT-LOVERS.jpg',
       price: 1.99,
       quantity: 1000
     },
     {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
+      name: "Eggs Florentine Benedict",
+      category: categories[1]._id,
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
+      image: 'EGGS-FLORENTINE.jpg',
       price: 2.99,
       quantity: 1000
     },
     {
-      name: 'Teddy Bear',
-      category: categories[4]._id,
+      name: "Traditional Buttermilk Pancakes",
+      category: categories[2]._id,
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
+      image: 'TRADITIONAL-BUTTERMILK-PANCAKES.jpg',
       price: 7.99,
       quantity: 100
     },
     {
-      name: 'Alphabet Blocks',
+      name: "Chocolate Chip Pancakes",
+      category: categories[2]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'CHOCO-CHIP-PANCAKES.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Strawberry Banana Pancakes",
+      category: categories[2]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'STRAWBERRY-BANANA-PANCAKES.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Very Blueberry Pancakes",
+      category: categories[2]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'ES-BLUEBERRY-PANCAKES-1024x683.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Skyscraper Pancake",
+      category: categories[2]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'ES-SKYSCRAPER_PANCAKES.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Going Green Smoothie",
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'green-smoothie-1024x683.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "MMMMango Smoothie",
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'Mango-1024x683.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Winning! Smoothie",
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'winning-smoothie-1024x683.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Strawberry Banana Smoothie",
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'strawberry-banana-smoothie-1024x683.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Signature Coffee",
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'ES-COFFEE-1.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Iced Coffee",
+      category: categories[3]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'Iced-Coffee-1024x683.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+    {
+      name: "Side of Fruit",
       category: categories[4]._id,
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
       image: 'alphabet-blocks.jpg',
       price: 9.99,
       quantity: 600
-    }
+    },
+    {
+      name: "Extra toast",
+      category: categories[4]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'alphabet-blocks.jpg',
+      price: 9.99,
+      quantity: 600
+    },
+
   ]);
 
   console.log('products seeded');
