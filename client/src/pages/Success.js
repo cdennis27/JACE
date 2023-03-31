070202f8102141b8abd0bc08ec3e2f1db30ff805
yerimechoi <yerimechoi@gmail.com>
2023-03-31 10:10:14 -0400

    Edited success page again

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -33,11 +33,11 @@ function Success() {
   }, [addOrder]);
 
   return (
-    <div>
+    <div className="success-children">
       <Jumbotron>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
+        <h2>💸👍</h2>
+        <h2>Your server will bring your order when ready!</h2>
+        <h4>You will now be redirected to the home page</h4>
       </Jumbotron>
     </div>
   );
